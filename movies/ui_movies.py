3a76324966d86b8825a867b6a09f4617b333a3dc
simpletech310 <34330714+simpletech310@users.noreply.github.com>
2020-10-28 12:00:35 -0700

    Configured add movie feature

--- movies/ui_movies.py
+++ movies/ui_movies.py
@@ -1,5 +1,34 @@
+from os import system
+import time
+import ui.main as ui
+import csv
+
 def call_add_movie():
-    pass
+    ui.print_header()
+    print('Add Movie')
+    print()
+    movie_title = input("Movie Title: ")
+    movie_release_date = input("Movie Release Date: ")
+    rating = input("Rating: ")
+    number_of_units = input("How many do you have in stock?: ")
+
+    fields = ['movie_title', 'movie_release_date', 'rating', 'number_of_units']
+    rows = [[movie_title, movie_release_date, rating, number_of_units]]
+
+    with open("data/movie.csv", 'a') as csvfile:
+       # creating a csv writer object  
+       csvwriter = csv.writer(csvfile) 
+
+       if csvfile.tell() == 0:
+           # writing the fields  
+           csvwriter.writerow(fields)
+
+       # writing the data rows  
+       csvwriter.writerows(rows)
+    
+    system("clear")
+    print("New movie added")
+    time.sleep(3)
 
 def call_edit_movie():
     pass
